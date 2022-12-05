cf10f1eef59f73c68658ce853b4203e43097b11a
Nikumbh <ganesh.a.nikumbh@capgemini.com>
2022-12-05 16:11:53 +0530

    Initial Commit

---
+++ src/main/scala/io/github/ganeshnikumbh/splinecustomhttpdispatcher/Calculator.scala
@@ -0,0 +1,8 @@
+package io.github.ganeshnikumbh.splinecustomhttpdispatcher
+
+class Calculator {
+  def add(a: Int, b: Int) = a + b
+  def subtract(a: Int, b: Int) = a - b
+  def multiply(a: Int, b: Int) = a * b
+  def divide(a: Int, b: Int) = a / b
+}
