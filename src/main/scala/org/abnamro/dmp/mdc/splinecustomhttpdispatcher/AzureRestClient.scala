f49c96756e89971c2c1a1008b64f1ca5a54065da
Ganesh Nikumbh <C83775@launcher.int.abnamro.com>
2023-02-16 13:53:21 +0100

    changed custom jar

---
+++ src/main/scala/org/abnamro/dmp/mdc/splinecustomhttpdispatcher/AzureRestClient.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.abnamro.dmp.mdc.splinecustomhttpdispatcher
+
+import org.apache.spark.internal.Logging
+import scalaj.http.HttpOptions.HttpOption
+import scalaj.http.{BaseHttp, HttpOptions}
+import za.co.absa.commons.lang.extensions.AnyExtension._
+import za.co.absa.spline.harvester.dispatcher.SplineHeaders
+
+import scala.concurrent.duration.Duration
+
+trait AzureRestClient {
+  def endpoint(url: String): AzureRestEndpoint
+}
+
+object AzureRestClient extends Logging {
+  /**
+   * @param baseHttp    HTTP client
+   * @param baseURL     REST endpoint base URL
+   * @param connTimeout timeout for establishing TCP connection
+   * @param readTimeout timeout for each individual TCP packet (in already established connection)
+   */
+  def apply(
+    baseHttp: BaseHttp,
+    baseURL: String,
+    connTimeout: Duration,
+    readTimeout: Duration,
+    disableSslValidation: Boolean,
+    headers: Map[String, String],
+    authentication: Map[String, String]
+  ): AzureRestClient = {
+
+    logDebug(s"baseURL = $baseURL")
+    logDebug(s"connTimeout = $connTimeout")
+    logDebug(s"readTimeout = $readTimeout")
+    logDebug(s"disableSslValidation = $disableSslValidation")
+    logDebug(s"headers = $headers")
+
+    val maybeDisableSslValidationOption: Option[HttpOption] =
+      if (disableSslValidation) {
+        logWarning(s"SSL validation is DISABLED -- not recommended for production!")
+        Some(HttpOptions.allowUnsafeSSL)
+      } else {
+        None
+      }
+
+    //noinspection ConvertExpressionToSAM
+    new AzureRestClient {
+      override def endpoint(resource: String): AzureRestEndpoint = new AzureRestEndpoint(
+        baseHttp(s"$baseURL/$resource")
+          .option(HttpOptions.connTimeout(connTimeout.toMillis.toInt))
+          .option(HttpOptions.readTimeout(readTimeout.toMillis.toInt))
+          .having(maybeDisableSslValidationOption)(_ option _)
+          .header(SplineHeaders.Timeout, readTimeout.toMillis.toString)
+          .headers(headers)
+          .compress(true),authentication)
+    }
+  }
+}
